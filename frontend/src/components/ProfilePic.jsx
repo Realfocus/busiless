8751d99bd9019e60809c4e60f353a55a15bdcd86
dezmymachine <desmondkwakucoffie195@gmail.com>
2025-05-19 19:45:26 +0000

    bug fix

--- frontend/src/components/ProfilePic.jsx
+++ frontend/src/components/ProfilePic.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import placeholderImage from "../assets/logo.png"
 
 const ProfilePic = () => {
   const [name, setName] = useState("");
@@ -8,21 +9,21 @@ const ProfilePic = () => {
     const accessToken = sessionStorage.getItem("loginToken");
     if (accessToken) {
       setName(sessionStorage.getItem("fullName") || "");
-      setPic(sessionStorage.getItem("image") || "");
+      setPic(sessionStorage.getItem("image") || placeholderImage);
     }
   }, []);
 
   return (
     <div className="flex items-center gap-2 p-2">
-  <div className="flex-shrink-0">
-    <img
-      src={pic}
-      alt={`Profile picture of ${name}`}
-      className="rounded-full w-8 h-8 object-cover border border-gray-200"
-    />
-  </div>
-  <p className="font-medium text-sm truncate">{name}</p>
-</div>
+      <div className="flex-shrink-0">
+        <img
+          src={pic}
+          alt={`Profile picture of ${name}`}
+          className="rounded-full w-8 h-8 object-cover border border-gray-200"
+        />
+      </div>
+      <p className="font-medium text-sm truncate">{name}</p>
+    </div>
   );
 };
 
